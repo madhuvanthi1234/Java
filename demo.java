5dd7bf0fcea78aac3ca5c7815901a3d1783f8bfc
madhuvanthi1234 <madhuvanthi272005@gmail.com>
2024-11-30 04:47:38 -0800

    new

--- demo.java
+++ demo.java
@@ -1,10 +1,9 @@
- import java.util.scanner;
+ import java.util.Scanner;
  class demo{
     public static void main (String args[]){
-        Scanner mv = new Scanner (String.in);
+        Scanner mv = new Scanner (System.in);
         String a = mv.nextLine();
         int b = mv.nextInt();
-
         System.out.println(a);
         System.out.println(b);
     }
